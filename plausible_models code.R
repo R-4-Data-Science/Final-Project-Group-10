f2a4f3a8ec479e81146386bcf38b7e91d2df2014
JoeVollenweider <joe_vollenweider@yahoo.com>
2025-12-04 10:01:54 -0600

    Finished the first part of 3.3

--- plausible_models code.R
+++ plausible_models code.R
@@ -1,5 +1,26 @@
-AICmin <- 10e-6
+# 3.3
+# https://chatgpt.com/share/6931ad54-5900-8011-a70d-63be4254d81c
+
 
 plausible_models <- function(path_forest, path_stability, delta=2, tau=0.6) {
+  
+  AICdf <- data.frame(model=names(path_forest$aic_by_model), AIC=unlist(path_forest$aic_by_model), vars=I(strsplit(names(path_forest$aic_by_model), "\\+")))
+  kept_models_index <- c()
+  AICMin <- min(AICdf$AIC)
+  
+  for (i in 1:length(AICdf$model)) {
+    if (AICdf$AIC[i] <= AICMin + delta) {
+      kept_models_index <- c(kept_models_index, i)
+    }
+  }
+  
+  kept_models <- AICdf[kept_models_index,]
+  
+  stability <- NA
+  kept_models <- data.frame(kept_models, stability)
+  
+  for (m in 1:length(kept_models$model)) {
+    
+  }
   
 }
